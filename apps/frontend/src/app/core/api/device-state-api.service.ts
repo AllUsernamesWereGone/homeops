15d4bb5beb603e40d1c0ffc25c40dd4828c452cb
AllUsernamesWereGone <145951324+AllUsernamesWereGone@users.noreply.github.com>
2026-07-05 20:38:31 +0200

    added neo openapi endpoint and values

--- apps/frontend/src/app/core/api/device-state-api.service.ts
+++ apps/frontend/src/app/core/api/device-state-api.service.ts
@@ -1,1 +1,26 @@
-//TODO
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+import {ApiConfiguration} from '../../api/api-configuration';
+import {findLatestStateByDeviceId} from '../../api/functions';
+import {DeviceTelemetryStateDto} from '../../api/models/device-telemetry-state-dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeviceStateApiService {
+
+  constructor(
+    private http: HttpClient,
+    private apiConfiguration: ApiConfiguration
+  ) {
+  }
+
+  findLatestByDeviceId(deviceId: string): Observable<DeviceTelemetryStateDto> {
+    return findLatestStateByDeviceId(this.http, this.apiConfiguration.rootUrl, {deviceId}).pipe(
+      map(response => response.body)
+    );
+  }
+}
